5ebd6beb8a49be0b511467384b7ed26320371dcd
Parjan Ameen <parjan.ameen@gmail.com>
2023-08-21 22:54:24 -0400

    Testing link

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "./ParjanAmeen.github.io",
-  base: '/',
+  base: "./ParjanAmeen.github.io",
   plugins: [react()],
 })
