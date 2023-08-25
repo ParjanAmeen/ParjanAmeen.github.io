350ee7142e11f6316a5410f730809a76796b22e6
Parjan ameen <parjan.ameen@gmail.com>
2023-08-25 12:41:59 -0400

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ function App() {
 
   return (
     <div className="App">
-
       <div className="search-bar-container">
         <SearchBar setResults={setResults} />
         {results && results.length > 0 && <SearchResultsList results={results} />}
